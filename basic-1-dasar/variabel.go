06d794481c8f93a36a1be89c4bae1660075f08ea
YammD123 <siamqueen52@gmail.com>
2025-07-12 01:05:34 +0700

    variable

---
+++ basic-1-dasar/variabel.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+
+func main() {
+	var name string
+	name = "siam"
+	age:= 16
+
+	var x,y int = 1,2
+	fmt.Println(x,y)
+	fmt.Println("Hello", name, "you are", age, "years old")
+}
